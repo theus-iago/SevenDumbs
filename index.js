0d60e2745472c831ff7b4779e5935485a8f2e83c
theus-iago <120859355+theus-iago@users.noreply.github.com>
2023-08-16 16:29:17 -0300

    up

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function chat() {
+    let chat = document.querySelector("#chat");
+    document.querySelector("#chat").style.cssText = "transition:opacity 2s; opacity:100%;";
+}
+function exit_chat() {
+    let chat = document.querySelector("#chat");
+    document.querySelector("#chat").style.cssText = "transition:opacity 2s; opacity:0%;";
+}
