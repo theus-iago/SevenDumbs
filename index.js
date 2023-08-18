56a21fb1c62ab780ddb2a019fd09fe910d51d995
theus-iago <120859355+theus-iago@users.noreply.github.com>
2023-08-18 09:40:51 -0300

    update

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function exit_chat() {
     let chat = document.querySelector("#chat");
     document.querySelector("#chat").style.cssText = "transition:opacity 2s; opacity:0%;";
 }
+    function jogar() {
+        let jogo = document.querySelector("#jogo");
+        jogo.style.opacity = 100; 
+        jogo.src = "https://theus-iago.github.io/Projeto-No-Body/";
+    }
